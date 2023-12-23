ae480acfe0c4ba1a3a60d7eed476fcef87ae0fa3
Alexandre MALFREYT <alexandre.malfreyt@universite-paris-saclay.fr>
2023-12-23 17:40:46 +0100

    Fixed 404

--- pages/404.php
+++ pages/404.php
@@ -11,23 +11,23 @@
         
         <?php
             $phrases = [
-                "Oups ! Cette page est plus perdue qu'une olive dans une pizza hawaïenne.",
+                "Oups ! Cette page est plus perdue qu'une olive dans une pizza hawaïenne. 🫒",
                 "Il semblerait que cette page ait glissé hors de notre four. 🍕",
-                "Comme une pâte trop fine, cette page ne peut pas être trouvée.",
-                "Cette page a été emportée plus vite qu'une part de margherita un samedi soir.",
-                "On dirait que vous avez cherché une recette secrète qui n'existe pas !",
-                "Page non trouvée. Elle a dû filer, comme le fromage sur une pizza chaude.",
-                "Oups ! Cette page est introuvable, peut-être cachée sous le fromage ?",
-                "Vous êtes tombé sur une croûte, pas sur la page souhaitée.",
-                "Cette page est absente... Peut-être partie livrer des pizzas ?",
-                "Comme une pizza sans sauce, quelque chose manque ici.",
-                "Cette page a été mangée par un monstre de la pizza.",
+                "Comme une pâte trop fine, cette page ne peut pas être trouvée. 🍕",
+                "Cette page a été emportée plus vite qu'une part de margherita un samedi soir. 🍕",
+                "On dirait que vous avez cherché une recette secrète qui n'existe pas ! 🕵️",
+                "Page non trouvée. Elle a dû filer, comme le fromage sur une pizza chaude. 🧀",
+                "Oups ! Cette page est introuvable, peut-être cachée sous le fromage ? 🧀",
+                "Vous êtes tombé sur une croûte, pas sur la page souhaitée. 🍕",
+                "Cette page est absente... Peut-être partie livrer des pizzas ? 🛵",
+                "Comme une pizza sans sauce, quelque chose manque ici. 🍅",
+                "Cette page a été mangée par un monstre de la pizza. 👻",
             ];
         ?>    
 
         <div style="text-align: center; margin-block: 25vh;">
             <h1>404</h1>
-            <p><?= $phrases[rand(0, count($phrases) - 1)] ?></p>
+            <h4><?= $phrases[rand(0, count($phrases) - 1)] ?></h4>
         </div>
 
         <?php include("../views/components/footer.php"); ?>
