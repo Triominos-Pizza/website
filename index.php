95c1b2beb18e8e6225261d487a21a5db36ab5644
Alexandre MALFREYT <alexandre.malfreyt@universite-paris-saclay.fr>
2023-12-24 11:04:46 +0100

    Changed ROOT_PATH to ROOT_URL in index.php

--- index.php
+++ index.php
@@ -13,11 +13,11 @@
     <body class="page-accueil">
         <?php include("./views/components/header.php"); ?>
         
-        <main style="background-image: url('<?= $ROOT_PATH ?>/assets/images/backgrounds/background-pizza.png');">
+        <main style="background-image: url('<?= $ROOT_URL ?>/assets/images/backgrounds/background-pizza.png');">
             <h1>Bienvenue chez Triomino's Pizza !</h1>
             <p>La première chaine de pizzerias ouvertes dans des IUTs ! 🍕</p>
             <div>
-                <a class="primary-button large-button" href='<?= $ROOT_PATH ?>/pages/order.php'>Commander maintenant 🢂</a>
+                <a class="primary-button large-button" href='<?= $ROOT_URL ?>/pages/order.php'>Commander maintenant 🢂</a>
             </div>
         </main>
 
