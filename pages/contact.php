34b2cb7faca6a4422cf9a4c78948666a59a62838
Alexandre MALFREYT <alexandre.malfreyt@universite-paris-saclay.fr>
2023-12-24 01:52:02 +0100

    Add contact page

---
+++ pages/contact.php
@@ -0,0 +1,22 @@
+<?php session_start(); ?>
+<?php include_once('../config/config.php'); ?>
+<?php require_once("../scripts/check_maintenance.php"); ?>
+<html>
+<?php
+$title = "Contact";
+include_once("../views/components/head.php");
+?>
+
+<body>
+    <?php include("../views/components/header.php"); ?>
+
+    <!-- Content -->
+        <main style="height: 40vh;">
+            <h1>Contact</h1>
+            <p>TODO</p>
+        </main>
+
+    <?php include("../views/components/footer.php"); ?>
+</body>
+
+</html>
