a81448ceca86288c37616aa9fd58a2c334953594
Alexandre MALFREYT <alexandre.malfreyt@universite-paris-saclay.fr>
2023-12-24 01:52:27 +0100

    Refactor product grid layout

--- pages/order.php
+++ pages/order.php
@@ -1,10 +1,7 @@
 <?php session_start(); ?>
 <?php include_once('../config/config.php'); ?>
 <?php require_once("../scripts/check_maintenance.php"); ?>
-<?php 
-    // require_once('../config/db.php');
-    // $connexion = connexion::connect();
-?>
+
 <html>
     <?php
         $title = "Commander";
