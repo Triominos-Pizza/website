7261731b8666844178075c570e4265cfb12e2788
Alexandre MALFREYT <alexandre.malfreyt@universite-paris-saclay.fr>
2023-12-27 15:57:46 +0100

    Add config, controller, and page templates

---
+++ controllers/controller.template.php
@@ -0,0 +1,14 @@
+<?php
+    require_once("../models/XXX.php");
+    require("../controllers/controllerObjet.php");
+
+class controllerXXX extends controllerObjet {
+        public function __construct() {
+            parent::__construct();
+        }
+
+        public static function showXXX() {
+        
+        }
+    }
+?>
