a46a4bd4cda102c6fb6518406f42dee3faec1c80
Alexandre MALFREYT <alexandre.malfreyt@universite-paris-saclay.fr>
2024-01-10 10:26:31 +0100

    Fixed maintenance redirect (when db is down or MAINTENANCE set to true)

--- config/db.php
+++ config/db.php
@@ -1,36 +1,42 @@
 <?php
-    class connexion{
-        // L'attribut static qui matérialise la connexion
-        private static PDO $pdo;
-        
-        // Le getter public de cet attribut
-        static public function pdo() {
-            return self::$pdo;
-        }
-        
-        // La fonction static de connexion qui initialise $pdo
-        // et lance la tentative de connexion
-        // PDO = PHP Data Object = une classe native adaptée à la connexion
-        static public function connect() {
-            try {
-                self::$pdo = new PDO(
-                    "mysql:host=".HOSTNAME.";dbname=".DATABASE, DB_LOGIN, DB_PASSWORD,
-                    array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
-                );
-                self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    if (!class_exists('connexion')) {
+
+        isset($CONFIG_IMPORTED) ? null : include('../config/config.php');
+
+        class connexion{
+            // L'attribut static qui matérialise la connexion
+            private static PDO $pdo;
+            
+            // Le getter public de cet attribut
+            static public function pdo() {
                 return self::$pdo;
-            } catch (PDOException $e) {
-                echo "Connexion échouée : " . $e->getMessage() . "<br/>";
-                return null;
+            }
+            
+            // La fonction static de connexion qui initialise $pdo
+            // et lance la tentative de connexion
+            // PDO = PHP Data Object = une classe native adaptée à la connexion
+            static public function connect() {
+                try {
+                    self::$pdo = new PDO(
+                        "mysql:host=".HOSTNAME.";dbname=".DATABASE, DB_LOGIN, DB_PASSWORD,
+                        array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
+                    );
+                    self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                    return self::$pdo;
+                } catch (PDOException $e) {
+                    echo "Connexion échouée : " . $e->getMessage() . "<br/>";
+                    return null;
+                }
+            }
+
+            static public function disconnect() {
+                self::$pdo = null;
+            }
+
+            static public function test(): bool {
+                return self::$pdo->exec("") !== false;
             }
         }
 
-        static public function disconnect() {
-            self::$pdo = null;
-        }
-
-        static public function test(): bool {
-            return self::$pdo->exec("") !== false;
-        }
     }
 ?>
--- scripts/php/check_maintenance.php
+++ scripts/php/check_maintenance.php
@@ -1,15 +1,15 @@
 <?php
     // Redirection en cas de maintenance (ou si la base de données est indisponible)
+    $existed = false;
+    include(FTP_ROOT_PATH . "/config/db.php");
+    $pdo = connexion::connect();
 
-    // require_once("../config/db.php");
-    // $pdo = connexion::connect();
-    
-    // if (MAINTENANCE || !$pdo) {
-    //     if (!$pdo) {
-    //         $reason = "La base de données est actuellement indisponible. Veuillez réessayer plus tard.";
-    //     } else {
-    //         $reason = "Maintenance";
-    //     }
-    //     header("Location: " . ROOT_PATH . "/pages/maintenance?reason=$reason");
-    // }
+    if (MAINTENANCE || !$pdo) {
+        if (!$pdo) {
+            $reason = "La base de données est actuellement indisponible. Veuillez réessayer plus tard.";
+        } else {
+            $reason = "Maintenance";
+        }
+        header("Location: " . ROOT_PATH . "/pages/maintenance.php?reason=$reason");
+    }
 ?>
