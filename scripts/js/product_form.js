0583879402b6232c4544d85cf6a1fc17cdf2a8f2
Alexandre MALFREYT <alexandre.malfreyt@universite-paris-saclay.fr>
2024-01-12 04:25:58 +0100

    Add event triggers for ingredient changes in product_form.js

--- scripts/js/product_form.js
+++ scripts/js/product_form.js
@@ -5,23 +5,32 @@ function incrQuantite() {
 
 function decrQuantite() {
     const elem = document.getElementById('quantite');
-    elem.value--;
+    if (elem.value > 1) {elem.value--;}
 }
 
 function incrIngredient(ingredient) {
     const elem = document.getElementById('ingredient-' + ingredient);
     elem.value++;
+
+    // trigger the onchange event to update the price
+    elem.dispatchEvent(new Event('change'));
 }
 
 function decrIngredient(ingredient) {
     const elem = document.getElementById('ingredient-' + ingredient);
-    elem.value--;
+    if (elem.value > 0) {elem.value--;}
+
+    // trigger the onchange event to update the price
+    elem.dispatchEvent(new Event('change'));
 }
 
 function resetIngredient(ingredient) {
     const elem = document.getElementById('ingredient-' + ingredient);
     const val = document.getElementById('ingredient-' + ingredient).defaultValue;
     elem.value = val;
+
+    // trigger the onchange event to update the price
+    elem.dispatchEvent(new Event('change'));
 }
 
 // return the price of the product (in the hidden input with name "prix")
@@ -30,11 +39,11 @@ function calculatePrice() {
     var price_elem = document.querySelector('input[name="price"]');
     var price = parseFloat(price_elem.value);
 
-    var selects = document.querySelectorAll('.product-form select');
-
     var total = price;
     console.log("Prix de base : " + total);
-
+    
+    // get the price of every option (sauce, pâte, taille)
+    var selects = document.querySelectorAll('.product-form select');
     for (var i = 0; i < selects.length; i++) {
         var selectedOption = selects[i].options[selects[i].selectedIndex];
         var regex = /\+(\d+\.\d{2}) €/;
@@ -45,6 +54,19 @@ function calculatePrice() {
             console.log("Prix de l'option " + i + " : " + optionPrice);
             total += optionPrice;
         }
+    }
+
+    // get the price of every ingredient added
+    var inputs = document.querySelectorAll('.product-form-ingredient input[id^=ingredient-]')
+    for (var i = 0; i < inputs.length; i++) {
+        var idIngredient = inputs[i].id.split('-')[1];
+        console.log("idIngredient : " + idIngredient);
+        console.log(`ingredient-${idIngredient}-price`);
+        var ingredientPrice = document.querySelector('input#ingredient-' + idIngredient + '-price').value;
+        var ingredientQuantity = Math.max(parseFloat(inputs[i].value) - parseFloat(inputs[i].defaultValue), 0);
+        console.log("Prix de l'ingrédient " + i + " : " + ingredientPrice);
+        console.log("Quantité de l'ingrédient " + i + " : " + ingredientQuantity);
+        total += ingredientPrice * ingredientQuantity;
     }
 
     if (!isNaN(total)) {
@@ -58,11 +80,19 @@ function calculatePrice() {
 }
 
 
-// trigger the function on load and on change of the "select" elements
+// trigger the function on load
 window.onload = calculatePrice;
+
+// trigger the function on change of the "select" elements for the options (sauce, pâte, taille)
 var selects = document.querySelectorAll('.product-form select');
 for (var i = 0; i < selects.length; i++) {
     selects[i].onchange = calculatePrice;
+}
+
+// Trigger the function on change of the "input" elements for the ingredients
+var inputs = document.querySelectorAll('.product-form-ingredient input[id^=ingredient-]')
+for (var i = 0; i < inputs.length; i++) {
+    inputs[i].onchange = calculatePrice;
 }
 
 // Apply .fade-right to the ingredients list if the list is not scrolled to the end
